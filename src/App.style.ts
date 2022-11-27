35df8a0613ec279119bad7dff8d781caba2700d8
henriquemaycon <mayconmarquesh@gmail.com>
2022-11-26 22:34:49 -0300

    finished and uploading to repot

--- src/App.style.ts
+++ src/App.style.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
     background-color: #24282f;
     min-height: 100vh;
-    color: #FFF;
 `
 
 export const Map = styled.div`
